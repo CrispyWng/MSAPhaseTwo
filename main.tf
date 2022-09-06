bafd6153eba3a07feb7cdf0310ed3d00c9f787b9
CrispyWng <fwon538@aucklanduni.ac.nz>
2022-09-06 17:04:37 +1200

    Changed name

--- main.tf
+++ main.tf
@@ -27,13 +27,13 @@ resource "random_integer" "ri" {
 
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup-${random_integer.ri.result}"
+  name     = "msaResource"
   location = "eastus"
 }
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-asp-${random_integer.ri.result}"
+  name                = "webapp-asp-msa"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
@@ -42,7 +42,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
-  name                = "webapp-${random_integer.ri.result}"
+  name                = "webapp-msa"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.appserviceplan.id
