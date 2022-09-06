ddcc03500872eb1291e6777b15827f173a6657e8
CrispyWng <fwon538@aucklanduni.ac.nz>
2022-09-06 20:42:36 +1200

    removed resource group and added state

--- main.tf
+++ main.tf
@@ -19,6 +19,13 @@ provider "azurerm" {
   client_secret   = "HTC8Q~.gF5UPV0nRcP2k-flb-AfcZsL9YjEqBb1C"
 }
 
+backend "azurerm" {
+  resource_group_name  = "resource-msa"
+  storage_account_name = "terraformstate23"
+  container_name       = "terraform"
+  key                  = "terraform.tfstate"
+}
+
 # Generate a random integer to create a globally unique name
 resource "random_integer" "ri" {
   min = 10000
@@ -26,16 +33,16 @@ resource "random_integer" "ri" {
 }
 
 # Create the resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "resource-msa"
-  location = "eastus"
-}
+# resource "azurerm_resource_group" "rg" {
+#   name     = "resource-msa"
+#   location = "eastus"
+# }
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
   name                = "webapp-asp-msa"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "eastus"
+  resource_group_name = "resource-msa"
   os_type             = "Linux"
   sku_name            = "F1"
 }
@@ -43,8 +50,8 @@ resource "azurerm_service_plan" "appserviceplan" {
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
   name                = "webapp-msa-crispy-wings"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "eastus"
+  resource_group_name = "resource-msa"
   service_plan_id     = azurerm_service_plan.appserviceplan.id
   https_only          = true
   site_config {
