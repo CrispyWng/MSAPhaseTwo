30ed4bdb764d7d3d5df0bf414e15fa0f01fe5773
CrispyWng <fwon538@aucklanduni.ac.nz>
2022-09-09 16:56:08 +1200

    Dockerfile done

--- backend/MSA.BackendAPI/MSA.BackendAPI/Program.cs
+++ backend/MSA.BackendAPI/MSA.BackendAPI/Program.cs
@@ -15,12 +15,13 @@ builder.Services.AddHttpClient(builder.Configuration["ClientName"], configureCli
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
